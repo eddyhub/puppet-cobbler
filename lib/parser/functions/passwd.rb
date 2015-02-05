cf2603f6f55b54134b9a8dff1d0883b737ddc224
eddyhub <eddyhub@users.noreply.github.com>
2015-02-05 22:45:23 +0100

    Adedd types and provider. Extended some types and providers with more options.

---
+++ lib/parser/functions/passwd.rb
@@ -0,0 +1,9 @@
+# Function: passwd
+# This function is used to create a password hash
+# algorithm: crypt
+# TODO: IMPLEMENT ME
+
+module Puppet::Parser::Functions
+  newfunction(:passwd) do |args|
+  end
+end
